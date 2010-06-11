bc7a61ad14ffa466f363dfccaa3fa1583d7eba57
Jeremy Carbaugh <jcarbaugh@gmail.com>
2010-06-11 14:33:36 -0400

    remove iscroll

--- js/news_source.js
+++ js/news_source.js
@@ -6,7 +6,6 @@ $(document).ready(function() {
 
 function loaded() {
 	document.addEventListener('touchmove', function(e){ e.preventDefault(); });
-	myScroll = new iScroll('scroller');
 }
 document.addEventListener('DOMContentLoaded', loaded);
 
