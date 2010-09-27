8545c39ac361c9858f19dc05b694e4e29e8a58b2
Joshua Ruihley <ruihley@gmail.com>
2010-09-27 17:58:31 -0400

    unstable

--- js/legislator_list_view.js
+++ js/legislator_list_view.js
@@ -1,8 +1,9 @@
 LegislatorListView.prototype = new View();
 function LegislatorListView() {
-    var self=this;
+    var self = this;
+}
     
-    self.addToLocal = function(row) {
+LegislatorListView.prototype.addToLocal = function(row) {
         application.localDb.transaction(
             function(transaction) {
                 transaction.executeSql("INSERT INTO Legislators (bioguide_id, is_favorite, website, firstname, lastname, congress_office, phone, webform, youtube_url, nickname, congresspedia_url, district, title, in_office, senate_class, name_suffix, twitter_id, birthdate, fec_id, state, crp_id, official_rss, gender, party, email, votesmart_id) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)", [row.bioguide_id, row.is_favorite, row.website, row.firstname, row.lastname, row.congress_office, row.phone, row.webform, row.youtube_url, row.nickname, row.congresspedia_url, row.district, row.title, row.in_office, row.senate_class, row.name_suffix, row.twitter_id, row.birthdate, row.fec_id, row.state, row.crp_id, row.official_rss, row.gender, row.party, row.email, row.votesmart_id]);
@@ -10,23 +11,24 @@ function LegislatorListView() {
         );
     }
     
-    self.loadLegislator = function(id, title) {
+LegislatorListView.prototype.loadLegislator = function(id, title, previous_view) {
+        alert(previous_view);
         localStorage.setItem("current_legislator", id);
         localStorage.setItem("current_legislator_title", title);
-        application.loadView('legislator', self.view_name);
+        application.loadView('legislator', previous_view);
     }
     
-    self.localToList = function(results) {
+LegislatorListView.prototype.localToList = function(results) {
         latest_list = [];
         for (var i=0; i<results.rows.length; i++) {
             var row = results.rows.item(i);
             (row.nickname == '') ? firstname=row.firstname : firstname=row.nickname;
-            latest_list.push({row_type:'content', id:row.id, title:row.title, firstname:firstname, lastname:row.lastname});
+            latest_list.push({row_type:'content', id:row.bioguide_id, title:row.title, firstname:firstname, lastname:row.lastname});
         }
         return latest_list;
     }
     
-    self.renderRow = function(row, dest_list) {
+LegislatorListView.prototype.renderRow = function(row, dest_list) {
         var newItem = document.createElement("li");
         var result = document.createElement("div");
         
@@ -38,7 +40,7 @@ function LegislatorListView() {
         
         var anchor = document.createElement("a");
     	$(anchor).click(function() {
-            self.loadLegislator(row.bioguide_id, row.title + '. ' + row.firstname + ' ' + row.lastname);
+            self.loadLegislator(row.id, row.title + '. ' + row.firstname + ' ' + row.lastname, this.view_name);
     	});
     	
         anchor.appendChild(titleDiv);
@@ -47,12 +49,10 @@ function LegislatorListView() {
         dest_list.appendChild(newItem);
     }
     
-    self.updateLegislator = function(row) {
+LegislatorListView.prototype.updateLegislator = function(row) {
         application.localDb.transaction(
             function(transaction) {
                 transaction.executeSql("UPDATE Legislators SET bioguide_id=?, website=?, firstname=?, lastname=?, congress_office=?, phone=?, webform=?, youtube_url=?, nickname=?, congresspedia_url=?, district=?, title=?, in_office=?, senate_class=?, name_suffix=?, twitter_id=?, birthdate=?, fec_id=?, state=?, crp_id=?, official_rss=?, gender=?, party=?, email=?, votesmart_id) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)", [row.bioguide_id, row.website, row.firstname, row.lastname, row.congress_office, row.phone, row.webform, row.youtube_url, row.nickname, row.congresspedia_url, row.district, row.title, row.in_office, row.senate_class, row.name_suffix, row.twitter_id, row.birthdate, row.fec_id, row.state, row.crp_id, row.official_rss, row.gender, row.party, row.email, row.votesmart_id]);
             }
         );
     }
-
-}
--- js/legislators/legislator.js
+++ js/legislators/legislator.js
@@ -4,11 +4,31 @@ function LegislatorView() {
     self.containerDiv = 'legislator_body';
     self.titleString = 'Legislator';
     self.subtitleString = '';
+    
+    self.dataHandler = function(transaction, results) {
+        alert(results.rows.item(i).bioguide_id);
+    }
+    
+    self.dbGetLatest = function(id) {
+        application.localDb.transaction(
+            function(transaction) {
+               transaction.executeSql("SELECT * FROM Legislators WHERE bioguide_id = ?", [id,], self.dataHandler);
+            }
+        );
+    }
 
     self.render = function(previous_view) {
+        alert(previous_view);
         self.setSubtitle(localStorage.getItem("current_legislator_title"));
         self.setTitle(self.titleString);
         self.setLeftButton('back', previous_view);
-        self.show();
+        self.loadThisLegislator(localStorage.getItem("current_legislator"));
+    }
+    
+    self.loadThisLegislator = function(id) {
+        self.dbGetLatest(id);
+        /*if (!application.isViewed('legislator_' + id)) {
+            self.serverGetLatest(id);
+        }*/
     }
 }
