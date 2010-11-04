21718c271cf6527d6d992f3a4e679b6b73e517e8
Joshua Ruihley <ruihley@gmail.com>
2010-11-04 14:22:44 -0400

    RELEASE 1.96

--- js/application.js
+++ js/application.js
@@ -1,6 +1,6 @@
 function Application() {
     this.title = "Real Time Congress";
-    this.version = "1.95";
+    this.version = "1.96";
     this.author = "Joshua Ruihley, Sunlight Foundation";
     this.copyright = "Copyright 2010, Sunlight Foundation";
     this.url = "http://realtimecongress.org";
